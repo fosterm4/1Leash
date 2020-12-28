34f8994d6c82d7aaf1ea73a7a378fc3df92a4629
Michael Foster <fosterm4@rams.colostate.edu>
2020-12-28 15:37:42 -0700

    headerfix

--- src/components/mobile-header.js
+++ src/components/mobile-header.js
@@ -1,30 +1,26 @@
-import React, {useContext} from "react"
+import React from "react"
+import SEO from "../components/seo"
 import { slide as MobileMenu } from 'react-burger-menu'
 import "./css/mobile-header.css"
 import logo from '../images/gatsby-icon.png'
 import bag from '../images/shopping-bag-black.png'
 import { Link } from "gatsby"
-import { SnipcartContext } from 'gatsby-plugin-snipcart-advanced/context';
 
-const MobileHeader = () => {
-  let buttonText = "SIGN IN";
-  const { state } = useContext(SnipcartContext);
-  const { userStatus } = state;
-  if (userStatus !== 'SignedOut') {
-    buttonText = "MY ACCOUNT";
+class MobileHeader extends React.Component {
+  render() {
+    return (
+      <div class="MobileMenu" style={{ display: "grid", gridTemplateColumns: "repeat(3, 1fr)", gridGap: 20 }}>
+        <MobileMenu>
+          <Link to="/aboutUs">ABOUT US</Link>
+          <Link to="/courses">BUY THE COURSE</Link>
+          <Link to="/shop">SHOP</Link>
+          <Link><button className="snipcart-customer-signin">SIGN IN</button></Link>
+        </MobileMenu>
+        <Link id="logo" to="/"><img src={logo} width="40" alt="logo" /></Link>
+        <Link id="shopping-bag"><img src={bag} width="40" alt="bag" className="snipcart-checkout"/></Link>
+      </div>
+    )
   }
-  return (
-    <div class="MobileMenu">
-      <MobileMenu>
-        <Link to="/aboutUs">ABOUT US</Link>
-        <Link to="/courses">BUY THE COURSE</Link>
-        <Link to="/shop">SHOP</Link>
-        <Link><button className="snipcart-customer-signin">{buttonText}</button></Link>
-      </MobileMenu>
-      <Link id="logo" to="/"><img src={logo} width="40" alt="logo" /></Link>
-      <Link id="shopping-bag"><img src={bag} width="40" alt="bag" className="snipcart-checkout" /></Link>
-    </div>
-  )
 }
 
 export default MobileHeader
--- src/components/web-header.js
+++ src/components/web-header.js
@@ -1,28 +1,26 @@
 import { Link } from "gatsby"
-import React, { useContext } from "react"
+import PropTypes from "prop-types"
+import React from "react"
 import logo from '../images/gatsby-icon.png'
 import bag from '../images/shopping-bag-black.png'
 import './css/web-header.css'
-import { SnipcartContext } from 'gatsby-plugin-snipcart-advanced/context';
 
-const WebHeader = () => {
-  let buttonText = "SIGN IN";
-  const { state } = useContext(SnipcartContext);
-  const { userStatus } = state;
-  if (userStatus !== 'SignedOut') {
-    buttonText = "MY ACCOUNT";
-  }
-    return (
-      <div className="Header">
+class WebHeader extends React.Component{
+
+  render(){
+    return(
+      <div className = "Header">
         <div className="HeaderGroup">
           <Link to="/aboutUs">ABOUT US</Link>
           <Link to="/courses">BUY THE COURSE</Link>
-          <Link to="/"><img src={logo} width="40" alt="logo" /></Link>
+          <Link to="/"><img src={logo} width="40" alt = "logo"/></Link>
           <Link to="/shop">SHOP</Link>
-          <Link><button className="snipcart-customer-signin">{buttonText}</button></Link>
-          <Link className="cart"><img src={bag} width="34" alt="bag" className="snipcart-checkout" /> </Link>
+          <Link><button className="snipcart-customer-signin">SIGN IN</button></Link>
+          <Link className="cart"><img src={bag} width="34" alt = "bag"className="snipcart-checkout"/> </Link>
         </div>
       </div>
     )
+  }
 }
 export default WebHeader
+
